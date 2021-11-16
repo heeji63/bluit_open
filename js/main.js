68ca8b840df8c77e3c085ccaa7d3fe02f9366e5b
heeji63 <69123a8721@gmail.com>
2021-11-16 19:14:05 +0900

    main/ contact 반응형 작업(+ contact 페이지 html백업후 작업중)

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
 $(document).ready(function() {
-  //섹션 스크롤
-  window.addEventListener("wheel", function(e){
-    e.preventDefault();
-  },{passive : false});
-  var mHtml = $("html");
-  var page = 1;
-  mHtml.animate({scrollTop : 0},10);
-  $(window).on("wheel", function(e) {
-    if(mHtml.is(":animated")) return;
-    if(e.originalEvent.deltaY > 0) {
-        if(page == 5) return;
-        page++;
-    } else if(e.originalEvent.deltaY < 0) {
-        if(page == 1) return;
-        page--;
-    }
-    var posTop =(page-1) * $(window).height();
-    mHtml.animate({scrollTop : posTop});
-  });
-
   var $menu = $('.gnb-ul li'),
       $menuMobile = $('.menu li'),
       $contents = $('.contents'),
